f21ab5de59b0ce080d6fdae37554d2495b999e9a
ShreyasUnnibhavi <shreyasunnibhavi5@gmail.com>
2025-12-07 20:03:09 +0530

    Create sumAB.java

---
+++ sumAB.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class sumAB{
+  public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a: ");
+    int a = sc.nextInt();
+    System.out.print("Enter b: ");
+    int b = sc.nextInt();
+    int sum = a + b;
+    System.out.print("The sum is: " + sum);
+  }
+}
