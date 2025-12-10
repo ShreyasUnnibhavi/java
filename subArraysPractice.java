436192af02bfd04d64acb4309b07fb3e88e9ca02
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2025-12-10 23:18:54 +0530

    Practice day commit (Kadenes algorithm)

--- subArraysPractice.java
+++ subArraysPractice.java
@@ -34,10 +34,38 @@ public class subArraysPractice {
         }
         System.out.print("Minimum sum: " + min + "\nMaximum sum: " + max);
     }
+
+    //function to calculate maximum sum of any subarray in a given array using kadane's algorithm
+    public static void kadanes(int array[]){
+        int curSum = 0;
+        int count = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i = 0; i < array.length; i++){
+            if(array[i] < 0){
+                count++;
+            }
+        }
+        for(int i = 0; i < array.length; i++){
+            if(count == array.length){
+                if(array[i] > max){
+                    max = array[i];
+                }
+            }else{
+                curSum = curSum + array[i];
+                if(curSum < 0){
+                    max = 0;
+                }else if(curSum > max){
+                    max = curSum;
+                }
+            }
+        }
+        System.out.print("Maximum sum: " + max);
+    }
     
     public static void main(String args[]){
-        int array[] = {2, 4, 6, 8, 10};
+        int array[] = {2,4,6,8,10};
         // makeSubArrays(array);
-        sumSubArray(array);
+        // sumSubArray(array);
+        kadanes(array);
     }
 }
