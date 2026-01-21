cf0fac0c6d867f0537e46e0ad39c8621c925399e
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-01-21 19:19:09 +0530

    mastering java day 21: Merging overlapping intervals (brute force) but faster [ref #1]

--- OverlappingIntervals.java
+++ OverlappingIntervals.java
@@ -2,30 +2,23 @@ import java.util.*;
 class Solution {
     public int[][] mergeIntervals(int intervals[][]){
         if(intervals.length <= 1) return intervals;
-        List<int[]> intervalsList = new ArrayList<>(Arrays.asList(intervals));
-        boolean mergeHpnd = true;
-         while(mergeHpnd){
-            List<int[]> temp = new ArrayList<>();
-            mergeHpnd = false;
+        List<int[]> list = new ArrayList<>(Arrays.asList(intervals));
+        List<int[]> result = new ArrayList<>();
+        while(!list.isEmpty()){
+            int current[] = list.remove(0);
+            
+            for(int i = 0; i < list.size(); i++){
+                int other[] = list.get(i);
 
-            while(!intervalsList.isEmpty()){
-                int[] current = intervalsList.remove(0);
-
-                for(int i = 0; i < intervalsList.size(); i++){
-                    int other[] = intervalsList.get(i);
-
-                    if(Math.max(current[0], other[0]) <= Math.min(current[1], other[1])){
-                        current = new int[]{Math.min(current[0], other[0]), Math.max(current[1], other[1])};
-                        intervalsList.remove(i);
-                        mergeHpnd = true;
-                        break;
-                    }
+                if(Math.max(current[0], other[0]) <= Math.min(current[1], other[1])){
+                    current = new int[]{Math.min(current[0], other[0]) , Math.max(current[1], other[1])};
+                    list.remove(i);
+                    i--;
                 }
-                temp.add(current);
             }
-            intervalsList = temp;
-         }
-        return intervalsList.toArray(new int[intervalsList.size()][]);
+            result.add(current);
+        }
+        return result.toArray(new int[list.size()][]);
     }
     public static void main(String[] args) {
         int intervals[][] = {{2,6},{1,3},{15,18},{8,10}};
