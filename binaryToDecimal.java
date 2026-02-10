c87b903935c3b40c86eec5caad0401d7bf612e92
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-02-10 16:41:08 +0530

    Corrected INdentation

--- binaryToDecimal.java
+++ binaryToDecimal.java
@@ -1,32 +1,13 @@
-// public class binaryToDecimal{
-//     public static int binToDec(int binary){
-//         int pow = 0;
-//         int decimal = 0;
-//         while(binary > 0){
-//             int lastDigit = binary % 10 ;
-//             decimal = decimal + (int)(lastDigit*Math.pow(2, pow));
-//             pow++;
-//             binary = binary / 10;
-//         }
-//         return decimal;
-//     }
-//     public static void main(String args[]){
-//         int binary = 1111;
-//         int decimal = binToDec(binary);
-//         System.out.print(decimal);
-//     }
-// }
-
 import java.util.*;
 
 public class binaryToDecimal{
     public static int generateDecimal(int num){
         int sum = 0;
-        int count = 0;
-        while(num > 0){
-            int n = num % 10;
-            sum = (int)(sum + n*(Math.pow(2,count)));
-            count++;
+        int mul = 1;
+        while(num > 0) {
+            int temp = num % 10;
+            sum = sum + temp * mul;
+            mul = mul * 2;
             num = num / 10;
         }
         return sum;
