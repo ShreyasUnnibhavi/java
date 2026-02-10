03d14c847c0c1722c69665144b33ed7a233b0a0a
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-02-10 23:45:02 +0530

    mastering java day 33: Finding the next permutation of a given sequence of numbers

--- NextPermutation.java
+++ NextPermutation.java
@@ -1,40 +1,37 @@
 public class NextPermutation {
     public static void nextPermutation(int[] nums) {
-        for(int i = nums.length-2; i >= 0; i--){
-            if(nums[i] < nums[i+1]){
-                int j = nums.length-1;
-                while(j > i){
-                    if(nums[j] > nums[i]){
-                        int temp = nums[i];
-                        nums[i] = nums[j];
-                        nums[j] = temp;
-                        break;
-                    }
-                    j--;
-                }
-                int k = nums.length-1;
-                reverse(nums,i+1,k);
-                break;
-            }else if(i == 0){
-                int k = nums.length-1;
-                reverse(nums,i,k);
-                break;
+        //^* 1. Finding the pivot
+        int i = nums.length-2;
+        while(i >= 0 && nums[i] >= nums[i+1]) {
+            i--;
+        }
+
+        //^* 2. swapping the pivot element with its strictly greater element to its right
+        if(i >= 0) {
+            int j = nums.length-1;
+            while(j > i && nums[j] < nums[i]) {
+                j--;
             }
+            swap(nums, i, j);
         }
-        printNums(nums);
-    }
-    static void reverse(int nums[], int i, int k){
-        while(i < k){
-            int temp = nums[k];
-            nums[k] = nums[i];
-            nums[i] = temp;
-            k--;
-            i++;
+
+        //^* 3. Reverse to get the next greater element/next permutation
+        reverse(nums, i+1, nums.length-1);
+
+        for(int k = 0; k < nums.length; k++) {
+            System.out.print(nums[k]);
         }
     }
-    static void printNums(int nums[]){
-        for(int i = 0; i < nums.length; i++){
-            System.out.print(nums[i] + " ");
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public static void reverse(int[] nums, int left, int right) {
+        while(left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
         }
     }
     public static void main(String[] args) {
