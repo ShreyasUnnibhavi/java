d3431c90bf5cb46a678f091c519706743ba59815
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-02-10 13:26:46 +0530

    mastering java day 33: Search an element in rotated sorted array

--- arrayPractice.java
+++ arrayPractice.java
@@ -1,28 +1,29 @@
 public class arrayPractice {
     public static int checkTarget(int nums[], int target){
-        int mid = 0;
-        for(int i = 1; i < nums.length-1; i++){
-            if(nums[i] > nums[i+1] && nums[i] > nums[i-1]){
-                mid = i;
-            }
-        }
-        int start = 0;
-        int end = nums.length-1;
-        while(start < end){
-            if(nums[mid] == target){
+        //! we are given a rotated sorted array
+        int left = 0;
+        int right = nums.length-1;
+        while(left <= right) {
+            int mid = right - (right-left)/2;
+            if(nums[mid] == target)
                 return mid;
-            }else if(target > nums[end]){
-                end = mid-1;
-            }else if(target < nums[start]){
-                start = mid+1;
+            if(nums[left] <= nums[mid]) {
+                if(target >= nums[left] && target < nums[mid]) 
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }else {
+                if(target > nums[mid] && target <= nums[right]) 
+                    left = mid + 1;
+                else
+                    right = mid - 1;
             }
-            mid = start + (end-start)/2;
         }
         return -1;
     }
     public static void main(String args[]){
-        int nums[] = {4, 5, 6, 7, 0, 1, 2};
-        int target = 9;
+        int nums[] = {5, 6, 0, 1, 2, 3, 4};
+        int target = 5;
         System.out.print(checkTarget(nums,target));
     }
 }
