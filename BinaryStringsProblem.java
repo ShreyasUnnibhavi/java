f95a77671b31a5d738bee25c2200c168e068296f
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-02-10 16:28:15 +0530

    Corrected Indentation

--- BinaryStringsProblem.java
+++ BinaryStringsProblem.java
@@ -1,12 +1,14 @@
 public class BinaryStringsProblem {
     public static void generateStrings(int n, int lastPlace, String str){
+        //base case
         if(n == 0) {
             System.out.println(str);
             return;
         }
-        generateStrings(n-1,0,str+"0");
-        if(lastPlace == 0){
-            generateStrings(n-1, 1, str+"1");
+        //recursion
+        generateStrings(n-1, 0, str + "0");
+        if(lastPlace == 0) {
+            generateStrings(n-1, 1, str + "1");
         }
     }
     public static void main(String[] args) {
