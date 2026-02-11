3dafd127268a568752d2735bf6f9d55afbe488e9
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-02-11 22:46:02 +0530

    Revisit

--- rainwater2.java
+++ rainwater2.java
@@ -1,22 +1,23 @@
 public class rainwater2{
     public static int trap(int[] height) {
-        int n = height.length;
-        int res = 0, l = 0, r = n - 1;
-        int rMax = height[r], lMax = height[l];
-        while (l < r) {
-            if (lMax < rMax) {
-                l++;
-                lMax = Math.max(lMax, height[l]);
-                res += lMax - height[l];
-            } else {    
-                r--;
-                rMax = Math.max(rMax, height[r]);
-                res += rMax - height[r];
+        int res = 0;
+        int left = 1, right = height.length-2;
+        int maxL = height[left], maxR = height[right];
+
+        while(left < right) {
+            if(height[left] < height[right]) {
+                left++;
+                maxL = Math.max(maxL, height[left]);
+                res += maxL - height[left];
+            }else {
+                right--;
+                maxR = Math.max(maxR, height[right]);
+                res += maxR - height[right];
             }
         }
         return res;
-}
-    public static void main(String args[]){
+    }
+    public static void main(String args[]) {
         int heights[] = {0, 1, 0,  2, 1, 0, 1, 3, 2, 1, 2, 1};
         System.out.print(trap(heights));
     }
