c2451d46b5e4a4d03334c9781d45449bbe55ad51
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-02-11 21:43:47 +0530

    mastering java day 33: Same logic in simple lines(MergeSortedArray)

--- ComputePowXn.java
+++ ComputePowXn.java
@@ -19,18 +19,3 @@ public class ComputePowXn {
         System.out.println(myPow(x, n));
     }
 }
-
-// long N = n;
-//         double ans = 1.0;
-//         if(N < 0){
-//             x = 1/x;
-//             N = -N;
-//         }
-//         while(N > 0){
-//             if((N & 1) == 1){
-//                 ans = ans * x;
-//             }
-//             x = x * x;
-//             N = N >> 1;
-//         }
-//         return ans;
