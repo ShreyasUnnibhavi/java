9d892d717654ff9ed1ace47c6a4f2703de55ab93
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-02-11 17:39:06 +0530

    mastering java: Friends pairing problem solved through recursion

--- FriendsPairingProblem.java
+++ FriendsPairingProblem.java
@@ -1,13 +1,17 @@
 public class FriendsPairingProblem {
     public static int pair(int n){
-        if(n == 1 || n == 2){
+        //^ Base case
+        if(n == 1 || n == 2) {
             return n;
         }
-        int fnm1 = pair(n-1); //first person staying single
-        int fnm2 = (n-1)*pair(n-2); //first person pairing with anyone out of the remaining 
+
+        //^ Recursion
+        int fnm1 = pair(n-1);           //& One friend decides to stay single, then we hav to solve for remaining n-1 firends
+        int fnm2 = (n-1) * pair(n-2);   //& One friend decides to pair up (can pair with remaining n-1) * solve for the remaining friends excpet these two (n-2)
+
         return fnm1 + fnm2;
     }
     public static void main(String[] args) {
-        System.out.println(pair(1));
+        System.out.println(pair(3));
     }
 }
