33408ed0cd975dedc5c32d8d44c41989cdcc9551
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-02-12 12:33:22 +0530

    mastering java day 34: Search in a 2D matrix by treating it as a 1D array

--- Search2dMatrix.java
+++ Search2dMatrix.java
@@ -1,24 +1,25 @@
 public class Search2dMatrix {
     public static boolean searchMatrix(int[][] matrix, int target) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        int low = 0;
-        int high = (rows*cols) - 1;
-        while(low <= high){
-            int mid = high - (high-low)/2;
-            int midVal = matrix[mid/cols][mid%cols];
-            if(target == midVal){
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int left = 0;
+        int right = n * m - 1;
+
+        while(left <= right) {
+            int mid = right - (right - left)/2;
+            int row = mid / n;
+            int col = mid % n;
+            if(matrix[row][col] == target) 
                 return true;
-            }else if(target < midVal){
-                high = mid - 1;
-            }else{
-                low = low + 1;
-            }
+            else if(target > matrix[row][col])
+                left = mid + 1;
+            else
+                right = mid - 1;
         }
         return false;
     }
     public static void main(String[] args) {
-        int matrix[][] = {{1,3,5,7},{10,11,16,20},{23,30,34,60}}, target = 39;
+        int matrix[][] = {{0,3,5,7},{10,11,16,20},{23,30,34,60}}, target = 100;
         System.out.println(searchMatrix(matrix, target));
     }
 }
