b0466a73edddb00454a9f089b86f8b2b5ee39c69
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-02-12 13:05:45 +0530

    Mastering java day 34: Understandin shallow and deep copy

--- ShallowDeep.java
+++ ShallowDeep.java
@@ -1,45 +1,43 @@
 public class ShallowDeep {
     public static void main(String[] args) {
-        Address address = new Address("Vijayapura");
+        Address adr = new Address("Vijayapura");
         Student s1 = new Student();
-        s1.name = "Shreyas";
-        s1.roll = 25;
-        s1.adr = address;
+        s1.name = "Shreyas" ;
+        s1.roll = 1;
+        s1.adr = adr;
         Student s2 = new Student(s1);
-        System.out.println(s2.name + " " + s2.roll + " " + s2.adr.city);
-        System.out.println(".......................................");
-        s2.adr.city = "muttagi";
-        s2.name = "shivu";
-        s2.roll = 1;
-        System.out.println(s2.name + " " + s2.roll + " " + s2.adr.city); 
-        System.out.println(s1.name + " " + s1.roll + " " + s1.adr.city); 
-
+        System.out.println(s1.name + ", " + s1.roll + ", " + s1.adr.city);
+        System.out.println(s2.name + ", " + s2.roll + ", " + s2.adr.city);
+        s2.name = "Shivu";
+        s2.roll = 2;
+        s2.adr.city = "Muttagi";
+        System.out.println(s1.name + ", " + s1.roll + ", " + s1.adr.city);
+        System.out.println(s2.name + ", " + s2.roll + ", " + s2.adr.city);
     }
 }
-class Address{
+
+class Address {
     String city;
-     Address(String otherAddress){
-        System.out.println("Adress constructor called");
-        this.city = otherAddress;
-     }
+    public Address(String adr) {
+        System.out.println("Address constructor");
+        this.city = adr;
+    }
 }
-class Student{
+
+class Student {
     String name;
     int roll;
     Address adr;
 
-    //normal/default constructor
-    Student(){
-        System.out.println("Default constructor created...");
+    Student() {
+        System.out.println("Defaut student constructor");
     }
-    
-    //copy constructor
-    Student(Student other){
-        System.out.println("copy contructor created");
+
+    Student(Student other) {
+        System.out.println("Copy constructor of Student");
         this.name = other.name;
         this.roll = other.roll;
-        this.adr = new Address(other.adr.city);    // deep copy
-        // this.adr = other.adr.city;              //shallow copy
+        // this.adr = other.adr;   //^ Shallow copy
+        this.adr = new Address(other.adr.city);  //^ Deep copy
     }
 }
-
