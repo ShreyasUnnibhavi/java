c1c6066f9ff1f147970ac8af99dc37774a6b3bcb
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-02-13 23:06:17 +0530

    Revisit

--- TowerOfHanoi.java
+++ TowerOfHanoi.java
@@ -1,15 +1,21 @@
 public class TowerOfHanoi {
-    public static void towerOfHanoi(int n, String src, String helper, String dest){
+    public static void towerOfHanoi(int n, String s, String h, String d) {
+        //? s = source
+        //? h = helper
+        //? d = destination
+        //^ Base case
         if(n == 1){
-            System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
+            System.out.println("transfer disk " + n + " from " + s + " to " + d);
             return;
         }
-        towerOfHanoi(n-1, src, dest, helper);
-        System.out.println("Transfer disk " + n + " from " + src + " to " + dest);
-        towerOfHanoi(n-1, helper, src, dest);
+        //^ Recursion
+        towerOfHanoi(n-1, s, d, h);
+        System.out.println("transfer disks " + n + " from " + s + " to " + d);
+        towerOfHanoi(n-1, h, s, d);
+
     }
     public static void main(String[] args) {
-        int n = 3;
+        int n = 4;  //Number of disks
         towerOfHanoi(n, "A", "B", "C");
     }
 }
