ba00e187792c82418121dcf1db4ad15a0c84e372
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-02-13 22:29:45 +0530

    Revisit

--- Subsequences.java
+++ Subsequences.java
@@ -1,11 +1,15 @@
 public class Subsequences {
-    public static void printSubsequences(String str, int i, String current){
-        if(i == str.length()) {
-            System.out.println(current);
+    public static void printSubsequences(String str, int i, String curr) {
+        //! Base case
+        if(i == str.length()){
+            if(curr == "")
+                System.out.println("Empty string");
+            System.out.println(curr);
             return;
         }
-        printSubsequences(str, i+1, current + str.charAt(i));
-        printSubsequences(str, i+1, current);
+        //! Recursion
+        printSubsequences(str, i+1, curr + str.charAt(i)); //? Include
+        printSubsequences(str, i+1, curr);
     }
     public static void main(String[] args) {
         printSubsequences("abc", 0, "");
