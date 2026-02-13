54e3c1c17a9f92bb83b4d0a2fa8c263bbd0d9eca
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-02-13 21:54:41 +0530

    Revisit

--- PushXtoTheEnd.java
+++ PushXtoTheEnd.java
@@ -1,13 +1,15 @@
 public class PushXtoTheEnd {
     public static String push(String str, char x) {
-        if(str.isEmpty()) return "";
-        char current = str.charAt(0);
+        //& Base case
+        if(str.length() == 0) 
+            return "";
+        //& recursion
+        char curr = str.charAt(0);
         String res = push(str.substring(1), x);
-        if(str.charAt(0) == x){
-            return res + current;
-        }else{
-            return current + res;
-        }
+        if(str.charAt(0) == x)
+            return res + curr;
+        else
+            return curr + res;
     }
     public static void main(String[] args) {
         System.out.println(push("axbxcxd",'x'));
