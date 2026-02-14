ab350f357d782f4b031d96bb93f26f86b2ad4944
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-02-14 21:44:09 +0530

    Revisit

--- GridWays.java
+++ GridWays.java
@@ -1,16 +1,15 @@
 public class GridWays {
-    public static int findWays(int n, int m, int row, int col){
-        if(row == n - 1 && col == m - 1){
+    public static int findWays(int rows, int cols, int i, int j) {
+        //* Base case
+        if(i == rows-1 && j == cols-1)
             return 1;
-        }
-        if(row == n || col == m){
+        if(i == rows || j == cols)
             return 0;
-        }
-        // 2 options
-        int ways1 = findWays(n, m, row+1, col);  //move down
-        int ways2 = findWays(n, m, row, col+1);  //move right
         
-        return ways1 + ways2;   //ways moving down + ways moving right
+        //* Recursion
+        int down = findWays(rows, cols, i+1, j);    //&ways moving down
+        int right = findWays(rows, cols, i, j+1);   //&ways moving right
+        return down + right;    //^Total ways
     }
     public static void main(String[] args) {
         int n = 3, m = 3;
