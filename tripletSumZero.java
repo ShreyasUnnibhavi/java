c8ef40577a05435ed073af53693607fb88aad8c7
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-02-16 15:02:33 +0530

    Revisit

--- tripletSumZero.java
+++ tripletSumZero.java
@@ -3,25 +3,25 @@ class Solution{
     public List<List<Integer>> generateTriplets(int nums[]){
         List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
-        for(int i = 0; i < nums.length-2; i++){
+        for(int i = 0; i < nums.length-2; i++) {
             if(i > 0 && nums[i] == nums[i - 1]) continue;
             int target = -nums[i];
             int left = i + 1;
-            int right = nums.length - 1 ;
-            while(left < right){
+            int right = nums.length - 1;
+            while(left < right) {
                 int sum = nums[left] + nums[right];
-                if(sum == target){
+                if(sum == target) {
                     list.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    while(left < right && nums[left] == nums[left+1]) left++;
-                    while(left < right && nums[right] == nums[right-1]) right--;
 
                     left++;
                     right--;
-                } 
 
-                else if(sum > target) right--;
-                else left++;
+                    while(left < right && nums[left] == nums[left+1]) left++;
+                    while(left < right && nums[right] == nums[right-1]) right--;
+                }else if(sum > target)
+                    right--;
+                else
+                    left++;
             }
         }
         return list;
