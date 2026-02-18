58e8bf745a4177d504ebfabe23d5e9a91e4778dd
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-02-18 23:49:33 +0530

    Revisit

--- NQueens.java
+++ NQueens.java
@@ -1,61 +1,64 @@
 public class NQueens {
-    public static boolean isSafe(char chessBoard[][], int row, int col){
-
-        //vertically upwards, col remains same
-        for(int i = row-1; i >= 0; i--){
-            if(chessBoard[i][col] == 'Q') return false;
+    public static boolean isSafe(char chessBoard[][], int row, int col) {
+        //* checking for 'Q' vertically upwards (only row decreases, same column)
+        for(int i = row-1; i >= 0; i--) {
+            if(chessBoard[i][col] == 'Q')
+                return false;
         }
 
-        //diagonally left, both row and col go on decreasing
-        for(int i = row-1, j = col-1; i >= 0 && j >= 0; i--, j--){
-            if(chessBoard[i][j] == 'Q') return false;
+        //* checking for 'Q' diagonally left (row and col both decrease)
+        for(int i = row-1, j = col-1; i >= 0 && j >= 0; i--, j--) {
+            if(chessBoard[i][j] == 'Q')
+                return false;
         }
 
-        //diagonally right, row decreases col increases
-        for(int i = row-1, j = col+1; i >= 0 && j < chessBoard.length; i--, j++){
-            if(chessBoard[i][j] == 'Q') return false;
+        //* checking for 'Q' diagonally right (row decrease, col increases)
+        for(int i = row-1, j = col+1; i >= 0 && j < chessBoard.length; i--, j++) {
+            if(chessBoard[i][j] == 'Q')
+                return false;
         }
 
         return true;
     }
-    public static void printBoard(char chessBoard[][]){
-        System.out.println("-------chess board -------");
-        for(int i = 0; i < chessBoard.length; i++){
-            for(int j = 0; j < chessBoard.length; j++){
-                System.out.print(chessBoard[i][j] + " ");
+    public static boolean insertQueen(char chessBoard[][], int row) {
+        //! Base case
+        if(row == chessBoard.length){
+            return true;
+        } 
+
+        //! Recursive logic
+        for(int i = 0; i < chessBoard.length; i++) {
+            if(isSafe(chessBoard, row, i)){
+                chessBoard[row][i] = 'Q';
+                if(insertQueen(chessBoard, row+1))
+                    return true;
+                chessBoard[row][i] = '.';
+            }
+        }
+
+        return false;
+    }
+    public static void printBoard(char arr[][]) {
+        int n = arr.length;
+        System.out.println("***************CHESS BOARD***************");
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
     }
-    public static boolean nQueens(char chessBoard[][], int row){
-        if(row == chessBoard.length){
-            // printBoard(chessBoard);
-            return true;
-        }
-        for(int j = 0; j < chessBoard.length; j++){
-            if(isSafe(chessBoard, row, j)){
-                chessBoard[row][j] = 'Q' ;
-                if(nQueens(chessBoard, row+1)){
-                    return true;
-                }
-                chessBoard[row][j] = '.';
-            }
-        }
-        return false;
-    }
     public static void main(String[] args) {
-        int n = 3;
+        int n = 5;
         char chessBoard[][] = new char[n][n];
-        for(int i = 0; i < chessBoard.length; i++){
-            for(int j = 0; j < chessBoard.length; j++){
+        for(int i = 0; i < chessBoard.length; i++) {
+            for(int j = 0; j < chessBoard.length; j++) {
                 chessBoard[i][j] = '.';
             }
         }
-        if(nQueens(chessBoard, 0)){
-            System.out.println("Solution is possible");
+        if(insertQueen(chessBoard, 0)) 
             printBoard(chessBoard);
-        }else{
-            System.out.println("There is no solution");
-        }
+        else
+            System.out.println("Solution doesn't exist");
     }
 }
