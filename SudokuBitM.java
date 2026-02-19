a9612e145412eca2eedf9110c4b0f90f6128712e
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-02-19 16:03:16 +0530

    Revisit

--- SudokuBitM.java
+++ SudokuBitM.java
@@ -1,64 +1,64 @@
 public class SudokuBitM {
-    // arrays to store already present numbers
     static int rows[] = new int[9];
     static int cols[] = new int[9];
     static int box[] = new int[9];
-
-    public static void solveSudoku(char board[][]){
-        for(int i = 0; i < 9; i++){
-            for(int j = 0; j < 9; j++){
-                if(board[i][j] != '.'){
-                    int num = board[i][j] - '1';        //convert char to integer (0 to 8)
-                    int boxIndex = (i/3)*3 + (j/3);     //calculates the (3 X 3)box number
-                    rows[i] |= (1 << num);              //setting the ith bit, indicating its presence in the row
-                    cols[j] |= (1 << num);              //setting the ith bit, indicating its presence in the column
-                    box[boxIndex] |= (1 << num);        //setting the ith bit, indicating its presence in the box
-                }
-            }
-        }
-        placeNumbers(board, 0, 0);
-    }
-    public static boolean isValid(int boxIndex, int row, int col, int num){
-        int val = (1 << num);
-        
-        // returns true if the num is present in row, column or box
-        return ((rows[row] & val) != 0 || (cols[col] & val) != 0 || (box[boxIndex] & val) != 0);
-    }
-    public static void toggle(int row, int col, int boxIndex, int num){
+    public static void toggle(char board[][], int boxIndex, int row, int col, int num) {
         int mask = (1 << num);
-        
-        //sets or clears the ith bit (necessary for backtracking)
-        rows[row] ^= mask;      
+
+        rows[row] ^= mask;
         cols[col] ^= mask;
         box[boxIndex] ^= mask;
     }
-    public static boolean placeNumbers(char[][] board, int row, int col){
-        //base case
-        if(row == 9) return true;
-        //recursion
+    public static boolean isSafe(char board[][], int boxIndex, int row, int col, int num) {
+        int mask = (1 << num);
+
+        return ((rows[row] & mask) == 0) && ((cols[col] & mask) == 0) && ((box[boxIndex] & mask) == 0);
+    }
+    public static boolean solveSudoku(char board[][], int row, int col) {
+        //* Base case
+        if(row == 9)
+            return true;
+
+        //* Recusrion
         int nextRow = row;
         int nextCol = col + 1;
-        if(nextCol == 9){
+        if(nextCol == 9) {
             nextRow += 1;
             nextCol = 0;
         }
-
-        if(board[row][col] != '.') return placeNumbers(board, nextRow, nextCol);
-        int boxIndex = (row/3)*3 + (col/3);
-        for(int i = 0; i < 9; i++){
-            if(!isValid(boxIndex, row, col, i)){
+        if(board[row][col] != '.')
+            return solveSudoku(board, nextRow, nextCol);
+        
+        for(int i = 0; i < 9; i++) {
+            int boxIndex = (row/3)*3 + (col/3);
+            if(isSafe(board, boxIndex, row, col, i)) {
                 board[row][col] = (char) (i + '1');
-                toggle(row, col, boxIndex, i);
-                if(placeNumbers(board, nextRow, nextCol)) return true;
-                toggle(row, col, boxIndex, i);
+                toggle(board, boxIndex, row, col, i);
+                if(solveSudoku(board, nextRow, nextCol))
+                    return true;
+                toggle(board, boxIndex, row, col, i);
                 board[row][col] = '.';
             }
         }
         return false;
     }
-    public static void print(char[][] board){
+    public static void solve(char board[][]) {
         for(int i = 0; i < 9; i++) {
-            for(int j = 0; j < 9; j++){
+            for(int j = 0; j < 9; j++) {
+                if(board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    rows[i] |= (1 << num);
+                    cols[j] |= (1 << num);
+                    int boxIndex = (i/3)*3 + (j/3);
+                    box[boxIndex] |= (1 << num);
+                }
+            }
+        }
+        solveSudoku(board, 0, 0); 
+    }
+    public static void print(char board[][]) {
+        for(int i = 0; i < 9; i++) {
+            for(int j = 0; j < 9; j++) {
                 System.out.print(board[i][j] + " ");
             }
             System.out.println();
@@ -76,7 +76,7 @@ public class SudokuBitM {
             {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
             {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
         };
-        solveSudoku(board);
+        solve(board);
         print(board);
     }
 }
