9ab7c0aeccd5f2d5703b103b89add21285015469
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-04-09 17:32:47 +0530

    Code refactor to use modular function

---
+++ Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+    public static void main(String[] demo) {
+        var firstName = "Arjun";
+        // System.out.print(firstName);
+        var junaid = true;
+        var shreyas = true;
+        var khadd = false;
+        var a = 10;
+        var b = 20;
+        System.out.println(!(!(a<b) && (b>a)));
+    }
+}
