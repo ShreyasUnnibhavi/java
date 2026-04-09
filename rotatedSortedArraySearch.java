e128f1654008f800ad6ed2db5205876744d1234a
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-04-09 22:32:43 +0530

    Refactored the code for better readability

--- rotatedSortedArraySearch.java
+++ rotatedSortedArraySearch.java
@@ -1,30 +1,36 @@
-public class RotatedSortedArraySearch {
-    public static int search(int array[], int target){
+public class rotatedSortedArraySearch {
+    public static int findkey(int[] array, int key) {
         int left = 0;
         int right = array.length-1;
-        while(left <= right){
-            int mid = left + (right - left)/2;
-            if(array[mid] == target){
+        while(left <= right) {
+            int mid = right - (right-left)/2;
+            if(key == array[mid]) {
                 return mid;
             }
-            if(array[left] <= array[mid]){
-                if(target >= array[left] && target < array[mid]){
+            // Case 1: If left half is sorted
+            if(array[left] <= array[mid]) {
+                if(key >= array[left] && key <= array[mid])
                     right = mid - 1;
-                }else{
+                else
                     left = mid + 1;
-                }
-            }else {
-                if(target > array[mid] && target <= array[right]){
+            }
+            //Case 2: Right half is sorted
+            else {
+                if(key <= array[right] && key >= array[mid])
                     left = mid + 1;
-                }else{
+                else
                     right = mid - 1;
-                }
             }
         }
         return -1;
     }
-    public static void main(String args[]){
-        int array[] = {5, 6, 7, 8, 9, 1, 2, 3, 4};
-        System.out.print(search(array, 4));
+    public static void main(String[] args) {
+        int array[] = {5, 6, 7, 8, 9, 1, 2};
+        int key = 22;
+        int position = findkey(array, key);
+        if(position == -1)
+            System.out.println("The key does not exist");
+        else
+            System.out.println("The key is at index " + position);
     }
 }
