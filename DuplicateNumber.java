386e5295287a1ddea4b65c2f8138e128fc4b9e27
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-04-26 21:59:41 +0530

    Program to find the duplicate element in an array of size n containing n +1 elements in the range [1, n]

---
+++ DuplicateNumber.java
@@ -0,0 +1,22 @@
+public class DuplicateNumber {
+    public static int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+
+        slow = nums[0];
+        while(slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 4, 3, 4, 2};
+        System.out.println(findDuplicate(nums));
+    }
+}
