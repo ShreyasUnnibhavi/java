346c31888ad485a137362d2a44ff82eb972d24f7
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-04-29 01:48:52 +0530

    Program to find the smallest letter greater than target in a character array

---
+++ SmallestLetter.java
@@ -0,0 +1,20 @@
+public class SmallestLetter {
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int left = 0;
+        int right = letters.length-1;
+
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(letters[mid] > target) 
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        return letters[left % letters.length];
+    }
+    public static void main(String[] args) {
+        char[] letters = {'c', 'f', 'j'};
+        char target = 'j';
+        System.out.println(nextGreatestLetter(letters, target));
+    }
+}
