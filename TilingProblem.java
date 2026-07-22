e5e885946968ba14c267a8454eb44639347f890c
Shreyas Unnibhavi <shreyasunnibhavi5@gmail.com>
2026-07-23 00:47:14 +0530

    Tiling problem reduced to O(n) from O(n^2) using memoization/dp

--- TilingProblem.java
+++ TilingProblem.java
@@ -1,13 +1,16 @@
 //solved using recursion
 public class TilingProblem {
-    public static int tiling(int n){
+    public static int tiling(int n, int[] dp){
         if(n == 0 || n == 1) return 1;
-        int vertical = tiling(n-1);    //function to calculate the remaining n-1 ways when the first tile is placed vertically(2 x 1)
-        int horizontal = tiling(n-2);  //function to calculate the remaining n-2 ways when the first 2 tiles are placed horizontally(2 x 1)
-        return vertical + horizontal;   //total number of ways to place 2 X 1 tiles on the floor(2 x n)
+        if(dp[n] != 0) return dp[n];
+        int vertical = tiling(n-1, dp);    //function to calculate the remaining n-1 ways when the first tile is placed vertically(2 x 1)
+        int horizontal = tiling(n-2, dp);
+        dp[n] = vertical + horizontal;  //function to calculate the remaining n-2 ways when the first 2 tiles are placed horizontally(2 x 1)
+        return dp[n];   //total number of ways to place 2 X 1 tiles on the floor(2 x n)
     }
     public static void main(String[] args) {
         int n = 4;
-        System.out.println(tiling(n));
+        int[] dp = new int[n + 1];
+        System.out.println(tiling(n, dp));
     }
 }
